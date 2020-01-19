1e42dadf3a3a46263c1b25e9f891fd2bbbe4fc82
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-01-19 11:19:42 -0600

    adding more structure on how the game might work

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+// You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+// * If the player selects the correct answer, 
+//show a screen congratulating them for choosing the right option. /
+//After a few seconds, display the next question -- do this without user input.
+// * The scenario is similar for wrong answers and time-outs.
+//   * If the player runs out of time, tell the player that time's up and display the correct answer.
+// Wait a few seconds, then show the next question.
+//   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer./
+// Wait a few seconds, then show the next question.
+// * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+
+
+// list of Global varibles
+
+var correctAns = 0;
+var incorrectAns = 0;
+
+
+//----
+
+// start Trivia game function
+
+
+
+
+/// show the questions and list of answeres function and array
+// need an array to list the question, and allow for the function to read through with .operator process
+
+
+
+// verify the correct button was selected correct answer === radio button selected === true
+//function myFunction() {
+    //     var x = document.getElementById("myRadio").checked;
+    //     document.getElementById("demo").innerHTML = x;
+    //   }
+
+
+
+
+    // show questions and multiple choice answers on web page with radio slectors 
+        // need a timer for the user to select the correct answer
+    // what happens when the user selects correct answer
+    // what happens when the user slects wrong answer
+        // need a timer after wrong answer
+
+
+/// at the end of the 15 questions, show the results from the answeres 
+        
