653f2a92fc7baefef0fa3d9cd1a251f41048fde4
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-01-21 22:54:54 -0600

    adding more content to the Html css, finished the questions array, and having them land on the HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,12 +20,67 @@ var incorrectAns = 0;
 //----
 
 // start Trivia game function
-
-
-
+// need my start button to be removed from the page after "click" and start the game
+$('.startBtn').on('click', function () {
+    $('.startBtn').remove();
+    // need a for loop to run through each of my questions, and make them appear on the Html page
+    for (var i = 0; i < triviaQs.length; i++) {
+        $('#questDisplay').append('<h5>' + triviaQs[i].question + '</h5>');
+        // my second loop to revew the answers that the user can select
+        for (var j = 0; j < triviaQs[i].selections.length; j++) {
+            $("#questDisplay").append("<input type='radio' name='question-" + i + "' value='" + triviaQs[i].selections[j] + "'>" + triviaQs[i].selections[j])
+        }
+    }
+})
 
 /// show the questions and list of answeres function and array
 // need an array to list the question, and allow for the function to read through with .operator process
+var triviaQs = [{
+    question: "What goverment operative with severe memory loss did Matt Damon bring to the silver screen?",
+    selections: ["James Bond  ", "Austing Powers  ", "Ethan Hunt  ", "Jason Bourne "],
+    corrAns: "Jason Bourne"
+},
+{
+    question: "what toy was Mr.Potato Head hoping Andy would get for his birthday, in Toy Story?",
+    selections: ["T-rex ", "Gi-Joe ", "Mrs. Potato Head ", "Buzz Light-Year "],
+    corrAns: "Mrs. Potato Head"
+},
+{
+    question: "How many player make up each squad in a regulation game of Ultimate Frisbee?",
+    selections: ["Four-teen  ", "Two  ", "Eight  ", "Seven"],
+    corrAns: "Seven"
+},
+{
+    question: "What car accessories come in flashy varieties known as Dubs, Spinners and Sprees?",
+    selections: ["Speakers  ", "Power Seats  ", "Wheel-Rims ", "Tires"],
+    corrAns: "Wheel-Rims"
+},
+{
+    question: "Who held the NBA record as the youngest player to reach 2000 career points, before Lebron James?",
+    selections: ["Larry Bird  ", "Kobe Bryant  ", "Kobe Beef  ", "Jason Stark"],
+    corrAns: "Kobe Bryant"
+},
+{
+    question: "What actor earned his first Oscar nom in 25 years for Catch Me if you can?",
+    selections: ["Chris Walken  ", "Leo Dicaprio  ", "Brad Pitt  ", "Matt Damon  "],
+    corrAns: "Chris Walken"
+},
+{
+    question: "What newswoman landed a cameo as a prison guard, in Austin Powers in Goldmember?",
+    selections: ["Katie Couric  ", "Diane Sawyer  ", "Barbara Walters  ", "Erin Andrews"],
+    corrAns: "Katie Couric"
+},
+{
+    question: "Which young Jedi Knight becomes Darth Vader in Star Wars?",
+    selections: ["Padme Amidala  ", "Hans Solo  ", "Sheev Palpatine  ", "Anakin Skywalker"],
+    corrAns: "Anakin Skywalker"
+},
+{
+    question: "What bounty hunter captures Han Solo?",
+    selections: ["Jabba the Hutt  ", "Boba Fett  ", "Chewbacca  ", "Jar Jar Binks"],
+    corrAns: "Boba Fett"
+}
+];
 
 
 // need to show only one queston at a time
@@ -48,4 +103,3 @@ var incorrectAns = 0;
 
 
 /// at the end of the 15 questions, show the results from the answeres 
-        
